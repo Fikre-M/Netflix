136044ec9b5c59b87e4467e9ff87afa90c43843e
Fikre-M <fikreddu@gmail.com>
2025-02-17 11:43:22 -0500

    deployyyy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/netflix-clone/",
+  base: "/Netflix/",
   plugins: [react()],
   server: {
     port: 5000,

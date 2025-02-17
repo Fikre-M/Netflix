49b4d2bcd5d616625974fd17fc4f825ed64c1bc2
Fikre-M <fikreddu@gmail.com>
2025-02-17 11:20:39 -0500

    Trigger Netlify Redeploy

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Netflix/",
   plugins: [react()],
-  base: '/Netflix/',
   server: {
     port: 5000,
     open: true,
